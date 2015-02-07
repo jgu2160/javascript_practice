c078a4edb5fd256a126380fadaa907cd7b002e8b
Jeffrey Gu <jeffwgu@gmail.com>
2015-02-06 17:48:02 -0700

    foundation made. will work on reacting to active inputs

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-function createSentence(){
-  var chinese = ["忍","耐","是","一","种","美","德","。"]
-  var pinyin = "Rěnnài shì yì zhǒng měidé."
-  var tonelessPinyin = "rennai shi yizhong meide."
-  var englishTrans = "Patience is a virtue."
-  document.getElementById("chinese").innerHTML = chinese.join("");
+var chinese = ["忍","耐","是","一","种","美","德","。"]
+var pinyin = "Rěnnài shì yì zhǒng měidé."
+var englishTrans = "Patience is a virtue."
+
+function createInitialSentence(){
+  this.defaultChineseSentence();
+  this.defaultPinyinSentence();
+  this.defaultEnglishSentence();
+  $("#english").addClass("incorrect");
+  $( "#chinese-sentence span:nth-child(4)" ).addClass("correct");
+}
+
+function defaultChineseSentence(){
+  document.getElementById("chinese-sentence").innerHTML = null;
+  for (var i = 0, len = chinese.length; i < len; i++) {
+    $('<span id="' + i +'">' + chinese[i] + '</span>', {'class':'chinese-char'}).appendTo('#chinese-sentence');
+  }
+}
+
+function defaultPinyinSentence(){
   document.getElementById("pinyin").innerHTML = pinyin;
+}
+
+function defaultEnglishSentence(){
+  document.getElementById("english").innerHTML = englishTrans;
+}
+
+function defaultTonelessPinyinSentence(){
+  var tonelessPinyin = "rennai shi yizhong meide."
 }
 
 function compareSentence(){
@@ -17,5 +39,5 @@ function compareSentence(){
 //})
 
 $(document).ready(function (){
-  createSentence();
+  createInitialSentence();
 })
